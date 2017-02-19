f3a002f1ed1690430fe11cf6a01f1b6ea49b63be
akash_95 <akashdeepmandal1995@gmail.com>
2017-02-19 19:42:55 +0530

    First Commit

---
+++ spec/first-test-package-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import FirstTestPackageView from '../lib/first-test-package-view';
+
+describe('FirstTestPackageView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
